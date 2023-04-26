c666557dc83b9dc1c5562ec39e0e6e6ba5962795
Himanshu Bobade <himanshubobade96@gmail.com>
2023-04-26 16:13:29 +0530

    final changes

--- venv/online_judge/judge/views.py
+++ venv/online_judge/judge/views.py
@@ -1,16 +1,17 @@
-import subprocess
-
-from django.shortcuts import render, get_object_or_404
-from django.template import loader
-import docker
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Questions, Testcase
-# from .apps import execute_code
-from .docker_init import execute_code_in_docker_container 
+from django.template import loader
+from django.contrib import messages
+from .models import Questions, CodeSnippet, Testcase
+from django.http import JsonResponse
+from .forms import CodeSnippetForm
+from django.conf import settings
+import subprocess
+import json
+from typing import List
 
 
 def questions(request):
-    # Method to fetch all the questions from the DB.
     myquestions = Questions.objects.all()
     context = {
         'myquestions': myquestions,
@@ -19,7 +20,6 @@ def questions(request):
 
 
 def details(request, id, ):
-    # Method to fetch the details of the question.
     question_detail = get_object_or_404(Questions, id=id)
     context = {
         'question_detail': question_detail,
@@ -28,11 +28,42 @@ def details(request, id, ):
 
 
 def main(request):
-    #Method to render the main project.
     template = loader.get_template('main.html')
     return HttpResponse(template.render(request=request))
 
 
+def execute_code(code: str, program_input: str) -> str:
+    result = subprocess.run(['g++', '-x', 'c++', '-o', 'program', '-'], input=code.encode('utf-8'),
+                            capture_output=True)
+
+    if result.returncode == 0:
+        if program_input is not None:
+            result = subprocess.run(['./program'], input=program_input.encode('utf-8'), capture_output=True)
+            output = result.stdout.decode('utf-8')
+
+        else:
+            output = "No input provided"
+
+    else:
+        output = result.stderr.decode('utf-8')
+
+    return output
+
+def execute_code1(code: str, program_input: List[str]) -> List[str]:
+    result = subprocess.run(['g++', '-x', 'c++', '-o', 'program', '-'], input=code.encode('utf-8'),
+                            capture_output=True)
+
+    if result.returncode == 0:
+        outputs = []
+        for input_str in program_input:
+            result = subprocess.run(['./program'], input=input_str.encode('utf-8'), capture_output=True)
+            output = result.stdout.decode('utf-8').strip()
+            outputs.append(output)
+
+        return outputs
+
+    else:
+        return []
 
 def create_code_snippet(request, id):
     if request.method == 'POST':
@@ -42,20 +73,13 @@ def create_code_snippet(request, id):
             code = request.POST.get('code')
             program_input = request.POST.get('input')
 
-            # print(code)
-            # print(program_input)
-
             if code is not None:
-                image = 'second'
-                print(image)
-                output = execute_code_in_docker_container(image, code, program_input)
-                print(output)
-
+                output = execute_code(code, program_input)
             else:
-                output = "Please provide a valid code."
+                output = "No code provided"
 
             return HttpResponse(output)
-
+        
         else:
             output = "No code provided"
             return render(request, 'create_code_snippet.html', {'output': output, 'question': question})
@@ -63,39 +87,6 @@ def create_code_snippet(request, id):
     else:
         question = Questions.objects.get(id=id)
         return render(request, 'create_code_snippet.html', {'question': question, 'id': id})
-
-
-
-
-
-def execute_code1(code, input_lines):
-    # create a Docker client
-    client = docker.from_env()
-
-    # build the Docker image for the compiler
-    image, _ = client.images.build(path='/path/to/dockerfile', tag='cpp_compiler')
-
-    # mount the code directory as a volume
-    code_dir = '/path/to/code'
-    volumes = {code_dir: {'bind': '/code', 'mode': 'rw'}}
-
-    # compile the code using Docker
-    cmd = f"g++ -o /code/program -"
-    container = client.containers.run(image, command=cmd, volumes=volumes, stdin_open=True, tty=True, detach=True)
-
-    # write the input to stdin of the container
-    container.exec_run('echo "{}" | ./program'.format('\\n'.join(input_lines)), stdin=True)
-
-    # get the output of the execution
-    output = container.logs().decode('utf-8')
-
-    # stop and remove the container
-    container.stop()
-    container.remove()
-
-    # return the output as a list of lines
-    return output.strip().split('\n')
-
 
 def result(request, id):
     if request.method == 'POST':
@@ -105,58 +96,44 @@ def result(request, id):
         results = []
 
         if code is not None:
-            # create a Docker client
-            client = docker.from_env()
+            result = subprocess.run(['g++', '-x', 'c++', '-o', 'program', '-'], input=code.encode('utf-8'), capture_output=True)
+            if result.returncode == 0:
+                testcases = Testcase.objects.filter(question=question)
+                inputs_outputs = []
+                for testcase in testcases:
+                    inputs_outputs.append({
+                        'input': [str(x) for x in testcase.input['input']],
+                        'output': [str(x) for x in testcase.output['output']]
+                    })
 
-            # build the Docker image for the compiler
-            image, _ = client.images.build(path='/path/to/dockerfile', tag='cpp_compiler')
+                for i, io in enumerate(inputs_outputs):
+                    input_lines = io['input']
+                    expected_output_lines = io['output']
+                    actual_output_lines = execute_code1(code, input_lines)
 
-            # mount the code directory as a volume
-            code_dir = '/path/to/code'
-            volumes = {code_dir: {'bind': '/code', 'mode': 'rw'}}
+                    verdict = 'Accepted'
+                    for expected, actual in zip(expected_output_lines, actual_output_lines):
+                        if expected.strip() != actual.strip():
+                            verdict = 'Wrong Answer'
+                            break
 
-            # compile the code using Docker
-            cmd = f"g++ -o /code/program -"
-            container = client.containers.run(image, command=cmd, volumes=volumes, stdin_open=True, tty=True, detach=True)
+                    results.append({
+                        'id': i+1,
+                        'input': input_lines,
+                        'expected_output': expected_output_lines,
+                        'actual_output': actual_output_lines,
+                        'verdict': verdict
+                    })
 
-            # write the input to stdin of the container
-            testcases = Testcase.objects.filter(question=question)
-            inputs_outputs = []
-            for testcase in testcases:
-                inputs_outputs.append({
-                    'input': [str(x) for x in testcase.input['input']],
-                    'output': [str(x) for x in testcase.output['output']]
-                })
-            for io in inputs_outputs:
-                input_lines = io['input']
-                container.exec_run('echo "{}" | ./program'.format('\\n'.join(input_lines)), stdin=True)
-
-                # get the output of the execution
-                output = container.logs().decode('utf-8')
-
-                expected_output_lines = io['output']
-                actual_output_lines = output.strip().split('\n')
-
-                verdict = 'Accepted'
-                for expected, actual in zip(expected_output_lines, actual_output_lines):
-                    if expected.strip() != actual.strip():
-                        verdict = 'Wrong Answer'
-                        break
-
-                results.append({
-                    'input': input_lines,
-                    'expected_output': expected_output_lines,
-                    'actual_output': actual_output_lines,
-                    'verdict': verdict
-                })
-
-            # stop and remove the container
-            container.stop()
-            container.remove()
-
-            form_submitted = True
-            return render(request, 'result.html', {'results': results, 'question': question, 'form_submitted': form_submitted})
+                form_submitted = True
+                print(results)
+                return render(request, 'result.html', {'results': results, 'question': question, 'form_submitted': form_submitted})
+            else:
+                # Compilation error occurred
+                results.append({'verdict': 'Compilation Error'})
+                print(result.stderr)
+                return render(request, 'result.html', {'results': results, 'question': question})
         else:
             # No code submitted
             results.append({'verdict': 'No Code Submitted'})
-        return render(request, 'result.html', {'results': results, 'question': question})
+            return render(request, 'result.html', {'results': results, 'question': question})

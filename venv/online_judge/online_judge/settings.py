51a0955821e3ac9b9d05e1780d41d9ef6e4d4e20
Himanshu Bobade <himanshubobade96@gmail.com>
2023-06-07 18:58:04 +0530

    vercel good changes

--- venv/online_judge/online_judge/settings.py
+++ venv/online_judge/online_judge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t&y*!@vo^ylp$v^g5ra@zlr8*ezd-=w2m*@=@2k2sd+7=3j@te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 
 # Default primary key field type
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
